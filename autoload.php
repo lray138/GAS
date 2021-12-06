8fdffd3ce563fbb44d98c88b810ad4dee1fdfdb9
lray138 <57813289+lray138@users.noreply.github.com>
2021-12-06 18:27:43 -0500

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,7 +1,8 @@
 <?php
 
+/* this is probably 'funky' and should be updated for v1.0 */
 function GAS_autoloader($className) {
-    
+   	$className = str_replace("lray138\\GAS\\", "", $className);
   	$path = __DIR__ . "/" . str_replace("\\", DIRECTORY_SEPARATOR, trim($className, "\\")).".php";
 
 	if(file_exists($path)) {
