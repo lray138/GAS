bd8ac20ee50d71d08e50596fa43b9ec1ef2aa3cd
lray138 <57813289+lray138@users.noreply.github.com>
2021-12-06 17:46:01 -0500

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,12 @@
+<?php
+
+function GAS_autoloader($className) {
+    
+  	$path = __DIR__ . "/" . str_replace("\\", DIRECTORY_SEPARATOR, trim($className, "\\")).".php";
+
+	if(file_exists($path)) {
+		require_once $path;
+	}
+}
+
+spl_autoload_register('GAS_autoloader');
